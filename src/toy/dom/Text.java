a22ae681025bfd3183e8cb646daba4912752e77b
Rahul Rudragoudar <rr83019@gmail.com>
2018-06-12 11:36:58 +0530

    Add list of words to Text

--- src/toy/dom/Text.java
+++ src/toy/dom/Text.java
@@ -1,12 +1,20 @@
 package toy.dom;
 
+import java.awt.List;
+
 public class Text extends Node {
   String value;
+  List words;
   
   Text(String s) {
     nodeType = "text";
     tag = "text";
     value = s;
   }
-
+  
+  void createList() {
+	  for(String w: value.split(" ")) {
+		  words.add(w);
+	  }
+  }
 }
